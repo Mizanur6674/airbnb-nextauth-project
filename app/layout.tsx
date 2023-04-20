07b90d534d5feb8d0c2f6b9dbbd0b112d4674916
mizanur rahman <mizanurrahma6674@gmail.com>
2023-04-20 14:02:43 +0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import { Nunito } from "next/font/google";
+import Navbar from "@/app/components/navbar/Navbar";
+import RegisterModal from "@/app/components/modals/RegisterModal";
+
+import ToasterProvider from "@/app/providers/ToasterProvider";
+
+import "./globals.css";
+import ClientOnly from "./components/ClientOnly";
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
+      </body>
+    </html>
+  );
+}
